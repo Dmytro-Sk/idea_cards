59393c9ec1cce101c641ed447488db72e5863add
Dmytro-Sk <0971898920d@gmail.com>
2021-11-27 14:46:15 +0200

    Add Home Page templates

--- apps/home_page/urls.py
+++ apps/home_page/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 app_name = 'home_page'
-urlpatterns = []
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='home_page/home_page.html'), name='home-page')
+]
