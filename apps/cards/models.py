696f21f00893c8770f6c7ed53e31575087ffdb00
Dmytro-Sk <0971898920d@gmail.com>
2021-11-29 09:18:22 +0200

    Add Card models

--- apps/cards/models.py
+++ apps/cards/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Card(models.Model):
+    title = models.CharField('card title', max_length=100, unique=True)
+    body = models.TextField('idea')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    mod_date = models.DateTimeField(auto_now=True)
+    rating_enable = models.BooleanField()
+
+    def __str__(self):
+        return self.title
+
+
+class CardRating(models.Model):
+    card = models.ForeignKey(Card, on_delete=models.CASCADE)
+    star_from_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stars = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)], default=0)
+
+    def __str__(self):
+        return f'Rating for {self.card.title}'
